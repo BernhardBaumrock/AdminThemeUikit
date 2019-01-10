a2e0d93f14cd4d87e9028ba27878ee1bb05f87e9
Bernhard Baumrock <bernhard@baumrock.com>
2019-01-10 15:56:33 +0100

    modify footer

--- _footer.php
+++ _footer.php
@@ -12,25 +12,9 @@ if(!defined("PROCESSWIRE")) die();
 <!-- FOOTER -->
 <footer id='pw-footer' class='uk-margin'>
 	<div class='pw-container uk-container uk-container-expand'>
-		<div uk-grid>
-			<div class='uk-width-1-3@m uk-flex-last@m uk-text-right@m uk-text-center'>
-				<div id='pw-uk-debug-toggle' class='uk-text-small'></div>
-			</div>	
-			<div class='uk-width-2-3@m uk-flex-first@m uk-text-center uk-text-left@m'>
-				<p class='uk-margin-remove'>
-					ProcessWire 
-					<?php if($adminTheme->isLoggedIn): ?>
-					<small class='uk-text-small uk-text-muted'>
-						<?php 
-						echo $config->versionName . ' <!--v' . $config->systemVersion . '--> &copy; ' . date("Y");
-						if($adminTheme->isEditor && $config->advanced) {
-							echo "<br />" . $adminTheme->renderNavIcon('flask') . $this->_('Advanced Mode'); 
-						}
-						?>
-					</small>	
-					<?php endif; ?>
-				</p>
-			</div>	
+		<div class="uk-text-center">
+			<?= $uk->config->company ?> &copy; <?= date('Y') ?>
+			<?php echo $adminTheme->renderExtraMarkup('footer'); ?>
 		</div>	
 		<?php if($adminTheme->isSuperuser && $config->debug):
 			include($config->paths->wire . 'templates-admin/debug.inc'); ?>
@@ -40,6 +24,5 @@ if(!defined("PROCESSWIRE")) die();
 			</script>
 		<?php endif; ?>
 	</div>
-	<?php echo $adminTheme->renderExtraMarkup('footer'); ?>
 </footer>
 
